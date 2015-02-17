e11494659067e2639648f79c806c88f85dc02077
Scott Fleming <fleming@stsci.edu>
2015-02-16 19:02:33 -0500

    - Improved performance by about a factor of 4, still would like to run faster though.

--- spec_plots/utils/specutils/calc_covering_fraction.py
+++ spec_plots/utils/specutils/calc_covering_fraction.py
@@ -12,6 +12,23 @@ import numpy
 
 #--------------------
 
+def testisGrey(rgb):
+    return rgb[0] == rgb[1] and rgb[0] == rgb[2]
+
+#--------------------
+
+def count_blue_red(buf):
+    blue_count = 0 ; red_count = 0
+    for ii in xrange(0,len(buf),3):
+        if not testisGrey(buf[ii:ii+3]):
+            if buf[ii] > 0 and buf[ii+2] == 0:
+                red_count += 1
+            elif buf[ii+2] > 0:
+                blue_count += 1
+    return blue_count, red_count
+
+#--------------------
+
 def calc_covering_fraction(fig, subplots, subplot_num):
     """
     Calculates the ratio of pixels that contain plot data vs. background (empty) pixels by temporarily filling the background with RGB value = red and then identifying blue (plot data) pixels.  This ratio is used to determine whether to make the plot lines transparent or not for readability.
@@ -43,25 +60,10 @@ def calc_covering_fraction(fig, subplots, subplot_num):
 
     """ Draw the plot, convert into numpy array of RGB values. """
     fig.canvas.draw()
-    w,h = fig.canvas.get_width_height()
     buf = numpy.fromstring( fig.canvas.tostring_rgb(), dtype=numpy.uint8 )
-    buf.shape = (w, h, 3)
-    buf = numpy.roll( buf, 2, axis=1)
-    
-    """ Now count the number of "blue" vs. "red" pixels in the entire figure (which should just be this subplot since we hid all the other ones. """
-    blue_count = 0.
-    red_count = 0.
-    for dim1 in xrange(buf.shape[0]):
-        for dim2 in xrange(buf.shape[1]):
-            """ Any grey pixels have equal R, G, and B values, and almost always represnt the axis titles, boundaries, etc., and are ignored. """
-            isGrey = buf[dim1,dim2,0] == buf[dim1,dim2,1] and buf[dim1,dim2,0] == buf[dim1,dim2,2]
-            
-            """ We define ANY data point that has ANY blue component and is not grey as containing data. """
-            if buf[dim1,dim2,2] > 0 and not isGrey:
-                blue_count += 1.
-            elif buf[dim1,dim2,0] > 0 and buf[dim1,dim2,2] == 0 and not isGrey:
-                """ We define any empty plot pixel as containing ANY red component but absolutely NO blue component as background/empty plot pixels.  Note that the grid lines (even if present) would count as red pixels in this definition. """
-                red_count += 1.
+
+    """ Count the number of "blue" and "red" pixels. """
+    blue_count, red_count = count_blue_red(buf)
 
     """ If this is the last subplot, then make sure we make all the other subplots visible again. """
     if subplot_num == subplots.shape[0]-1:
@@ -70,6 +72,8 @@ def calc_covering_fraction(fig, subplots, subplot_num):
 
     """ Make the background color white again for this subplot. """
     subplots[subplot_num].set_axis_bgcolor('white')
+    
+    print blue_count, red_count
 
     return blue_count / (red_count+blue_count) * 100.
 
