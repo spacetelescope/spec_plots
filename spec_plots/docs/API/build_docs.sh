9ef840d642c5c3a30e15e3eb68cec2411661df79
Scott Fleming <fleming@stsci.edu>
2016-04-18 16:57:07 -0400

    rtd tests

--- spec_plots/docs/API/build_docs.sh
+++ spec_plots/docs/API/build_docs.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-sphinx-build -b html . _build/html
+# This works fine.
+ sphinx-build -T -E -b html -d _build/doctrees-readthedocs -D language=en . _build/html
+
+# This is what RTD does, need to get this working on my local machine.
+#sphinx-build -T -E -b doctest -d _build/doctrees-readthedocs -D language=en . _build/html
