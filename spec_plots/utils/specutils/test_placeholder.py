1ae17d80bf1f465b64753fbf0021f74864e95aca
Scott Fleming <fleming@stsci.edu>
2025-04-13 13:50:58 -0400

    adding placeholder unit test, ci files

---
+++ spec_plots/utils/specutils/test_placeholder.py
@@ -0,0 +1,11 @@
+"""
+.. module:: test_placeholder
+   :synopsis: Placeholder unit test until actual tests are implemented.
+
+.. moduleauthor:: Scott W. Fleming
+"""
+
+import pytest
+
+def test_placeholder():
+    assert 1 == 1
