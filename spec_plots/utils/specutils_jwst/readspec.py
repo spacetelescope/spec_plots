14d49bbe60daedec6365eaf3feb2495a5f5858e3
Scott Fleming <fleming@stsci.edu>
2025-07-02 00:16:48 -0400

    add support for new TSO format of x1dints, fixes #125

--- spec_plots/utils/specutils_jwst/readspec.py
+++ spec_plots/utils/specutils_jwst/readspec.py
@@ -49,41 +49,65 @@ def readspec(input_file):
             sys.exit("*** MAKE_JWST_SPEC_PREVIEWS ERROR: EXTRACT1D extension"
                          " not found in FITS file.")
 
+        # For time series observations (TSO), many exposures are included in
+        # a single EXTRACT1D. In this case, a preview plot is only made for
+        # a single exposure (the first one). Remake a simplified Table that
+        # contains the columns of interest as columns rather than lists in a
+        # row entry.
+        extract_one_row = False
+        if len(jwst_table["WAVELENGTH"].shape) == 2:
+            extract_one_row = True
+
         # Extract wavelength, fluxes, flux uncertainties, and DQ flags for
         # each segment.
         try:
-            wavelength_col = jwst_table["WAVELENGTH"]
+            if not extract_one_row:
+                wavelength_col = jwst_table["WAVELENGTH"].data
+            else:
+                wavelength_col = jwst_table[0]["WAVELENGTH"]
+            wavelength_unit = jwst_table["WAVELENGTH"].unit
         except KeyError:
             sys.exit("*** MAKE_JWST_SPEC_PREVIEWS ERROR: WAVELENGTH column not"
                   " found in first extension's binary table.")
-        wavelength_unit = wavelength_col.unit
 
         try:
-            flux_col = jwst_table["FLUX"]
+            if not extract_one_row:
+                flux_col = jwst_table["FLUX"].data
+            else:
+                flux_col = jwst_table[0]["FLUX"]
+            flux_unit = jwst_table["FLUX"].unit
         except KeyError:
             sys.exit("*** MAKE_JWST_SPEC_PREVIEWS ERROR: FLUX column not"
                          " found in first extension's binary table.")
-        flux_unit = flux_col.unit
 
         try:
-            fluxerr_col = jwst_table["ERROR"]
+            if not extract_one_row:
+                fluxerr_col = jwst_table["ERROR"].data
+            else:
+                fluxerr_col = jwst_table[0]["ERROR"]
         except KeyError:
             try:
-                fluxerr_col = jwst_table["FLUX_ERROR"]
+                if not extract_one_row:
+                    fluxerr_col = jwst_table["FLUX_ERROR"].data
+                else:
+                    fluxerr_col = jwst_table[0]["FLUX_ERROR"]
             except KeyError:
                 sys.exit("*** MAKE_JWST_SPEC_PREVIEWS ERROR: neither ERROR "
                           "nor FLUX_ERROR column found in first "
                           "extension's binary table.")
 
         try:
-            dq_col = jwst_table["DQ"]
+            if not extract_one_row:
+                dq_col = jwst_table["DQ"].data
+            else:
+                dq_col = jwst_table[0]["DQ"]
         except KeyError:
             sys.exit("*** MAKE_JWST_SPEC_PREVIEWS ERROR: DQ column not found"
                   " in first extension's binary table.")
 
         # Create JWSTSpectrum object.
-        return_spec = JWSTSpectrum(wavelength_col.data, flux_col.data,
-                                       fluxerr_col.data, dq_col.data,
+        return_spec = JWSTSpectrum(wavelength_col, flux_col,
+                                       fluxerr_col, dq_col,
                                        wavelength_unit, flux_unit,
                                        orig_file=input_file)
 
